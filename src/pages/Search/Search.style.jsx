4293848bdad94a50cbae913884779bedd8da7f65
Alec Lima <aleclimadev@gmail.com>
2023-03-23 16:12:41 -0300

    chore: turned into component

---
+++ src/pages/Search/Search.style.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+export const SearchContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    p {
+    margin-bottom: 30px;
+    }
+
+
+
+    .noposts {
+    text-align: center;
+}
+`
+
+
+
+
+
+
+
+
+ 
